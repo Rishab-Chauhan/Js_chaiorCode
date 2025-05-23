c03067da75f4223abb1b5dd91714850ba592c5bf
Rishab-Chauhan <133254179+Rishab-Chauhan@users.noreply.github.com>
2025-05-23 06:02:42 +0000

    dataTypes Done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 123456
+let accountEmail ="rishab@google.com"
+var accountPassword = "5252"
+accountCity = "Haridwar"
+
+accountEmail= "rc@gmail.com"
+accountPassword= "96969"
+accountCity = "Roorkee"
+let accountState;
+
+
+/*
+Never use var as its cause issue in block scope and functional scope
+
+*/
+//accountId = 5
+console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,9 @@
+console.log (3+3)
+console.log ("Rishab")
+let name = "Rishab"
+let car;
+let age = 12
+let bike = null;
+isloggedin = true;
+
+console.log(typeof null, true);
