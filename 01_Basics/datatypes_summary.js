dc324c728fd58d88fac7ba964998855037714293
Rishab-Chauhan <133254179+Rishab-Chauhan@users.noreply.github.com>
2025-05-25 07:09:13 +0000

    new files

--- 01_Basics/datatypes_summary.js
+++ 01_Basics/datatypes_summary.js
@@ -18,3 +18,25 @@ myfunction = function () {
 }
 
 myfunction();
+
+
+//++++++++++++++++++SOME MEMORY ALLOCATIONS+++++++++++++++++
+// Stack type sin this a copy 
+let myname ="Rishab"
+
+let myanothername= myname;
+myanothername = "benjamin"
+console.log(myname)
+console.log(myanothername)
+
+// Heap
+
+myuser1 = {
+    name: "RC",
+    email:"RC@gmail.com" 
+}
+myuser2=myuser1
+myuser2.email="Rishab@gmail.com"
+console.log(myuser1)
+console.log(myuser2)
+
