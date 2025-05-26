92e9496a3ef29720f25f080f3cc77c6fb9bccc7a
Rishab <rishabchauhan9634@gmail.com>
2025-05-26 09:52:56 +0530

    Maths watch this segment again last few mins

---
+++ 01_Basics/06_maths and Numbers.js
@@ -0,0 +1,19 @@
+const number = 42;
+console.log (number)// In this javascript automatically converts the number into number
+const balance = new Number(100); //In this javascript creates an object of numbers, which means its 100 percent a number
+console.log(balance);
+
+const hundred= 100000000
+console.log(hundred.toLocaleString())
+
+// ++++++++++++++++++++++++++MATHS++++++++++++++++++++
+
+
+// console.log(Math.PI); // 3.141592653589793
+// console.log(Math.abs(-4)); // Jsut use to get the absolute value
+// console.log(Math.round(5.4)); // 4
+// console.log(Math.min(5.4,7)); //
+// console.log(Math.max(5.4,7,12)); //
+
+console.log(Math.random()); // always return a random number between 0 and 1
+console.log(Math.random()*10+1)
